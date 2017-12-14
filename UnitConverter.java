f71104a974bfccafcc417c39a55a271ab0487f35
yarensoydan <33071519+yarensoydan@users.noreply.github.com>
2017-12-14 18:57:41 +0300

    Create UnitConverter.java

---
+++ UnitConverter.java
@@ -0,0 +1,15 @@
+package se2;
+
+
+public abstract class UnitConverter
+
+{
+  public UnitConverter() { }
+
+  public abstract double convert(double inValue);
+
+  public abstract String toString();
+
+  public abstract void print();
+
+}
